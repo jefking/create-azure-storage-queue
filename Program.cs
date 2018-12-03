49711b46a41f1ddc08dbb267eb6ffbfd27d277b4
Jef King <jefkin@microsoft.com>
2018-12-03 15:32:27 -0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace create_queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
